90ba1b6bcb7aa48ee1852c9fb336cfe7af96ab62
Александр Кононов <kononov@nanosemantics.ai>
2026-02-22 11:44:31 +0300

    4

--- bot/models/db.py
+++ bot/models/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import (
     Column, BigInteger, Integer, String, Text,
-    ForeignKey, TIMESTAMP, func
+    ForeignKey, TIMESTAMP, func, Index
 )
 from sqlalchemy.orm import declarative_base, relationship
 
@@ -12,6 +12,7 @@ class User(Base):
 
     id = Column(BigInteger, primary_key=True)
     name = Column(String(255), nullable=False)
+    gender = Column(String(255), nullable=False)
     age = Column(Integer, nullable=False)
     height = Column(Integer, nullable=False)
     weight = Column(Integer, nullable=False)
@@ -35,6 +36,14 @@ class UserRequest(Base):
     )
     request_text = Column(Text, nullable=False)
     response_text = Column(Text, nullable=False)
-    created_at = Column(TIMESTAMP, server_default=func.now(), nullable=False)
+    timestamp = Column(TIMESTAMP, server_default=func.now(), nullable=False)
 
     user = relationship("User", back_populates="requests")
+
+    __table_args__ = (
+        Index(
+            "idx_user_requests_user_id_timestamp_desc",
+            "user_id",
+            timestamp.desc()
+        ),
+    )
